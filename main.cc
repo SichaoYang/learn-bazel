27425d559461bb1cac9f62403f89650c61ab2997
Sichao Yang <sichaoyang@pony.ai>
2022-07-30 18:49:11 +0800

    Gflags.

--- main.cc
+++ main.cc
@@ -5,16 +5,42 @@
 #include "calculator/calculator.h"
 #include "interface/operator.pb.h"
 
-int main(int argc, char* argv[]) {
+#include "gflags/gflags.h"
+
+DEFINE_string(operator_type, "", "Operator type.");
+DEFINE_double(operand, 0.0, "First operand.");
+DEFINE_double(rhs_operand, 0.0, "Second operand for binary operation.");
+
+int main(int argc, char *argv[]) {
   google::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
 
   learn_bazel::Calculator<double> calculator;
-  std::cout << calculator.Calculate(interface::BinaryOperatorType::kAddition, 1.0, 2.0) << std::endl;
-  std::cout << calculator.Calculate(interface::BinaryOperatorType::kSubtraction, 1.0, 2.0) << std::endl;
-  std::cout << calculator.Calculate(interface::BinaryOperatorType::kMultiplication, 1.0, 2.0) << std::endl;
-  std::cout << calculator.Calculate(interface::BinaryOperatorType::kDivision, 1.0, 0.0) << std::endl;
-  std::cout << calculator.Calculate(interface::UnaryOperatorType::kMinus, 2.0) << std::endl;
+  if (FLAGS_operator_type == "Minus") {
+    std::cout << calculator.Calculate(interface::UnaryOperatorType::kMinus,
+                                      FLAGS_operand)
+              << std::endl;
+  } else if (FLAGS_operator_type == "Addition") {
+    std::cout << calculator.Calculate(interface::BinaryOperatorType::kAddition,
+                                      FLAGS_operand, FLAGS_rhs_operand)
+              << std::endl;
+  } else if (FLAGS_operator_type == "Subtraction") {
+    std::cout << calculator.Calculate(
+                     interface::BinaryOperatorType::kSubtraction, FLAGS_operand,
+                     FLAGS_rhs_operand)
+              << std::endl;
+  } else if (FLAGS_operator_type == "Multiplication") {
+    std::cout << calculator.Calculate(
+                     interface::BinaryOperatorType::kMultiplication,
+                     FLAGS_operand, FLAGS_rhs_operand)
+              << std::endl;
+  } else if (FLAGS_operator_type == "Division") {
+    std::cout << calculator.Calculate(interface::BinaryOperatorType::kDivision,
+                                      FLAGS_operand, FLAGS_rhs_operand)
+              << std::endl;
+  } else {
+    LOG(FATAL) << "Unsupported operator";
+  }
 
   return EXIT_SUCCESS;
 }
