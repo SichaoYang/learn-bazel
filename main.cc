f6ec13032b76076881d5c1b7a45a4ff24cc51ff6
Sichao Yang <sichaoyang@pony.ai>
2022-07-30 15:36:50 +0800

    Glog.

--- main.cc
+++ main.cc
@@ -5,11 +5,14 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+
   learn_bazel::Calculator<double> calculator;
   std::cout << calculator.Calculate(learn_bazel::BinaryOperatorType::kAddition, 1.0, 2.0) << std::endl;
   std::cout << calculator.Calculate(learn_bazel::BinaryOperatorType::kSubtraction, 1.0, 2.0) << std::endl;
   std::cout << calculator.Calculate(learn_bazel::BinaryOperatorType::kMultiplication, 1.0, 2.0) << std::endl;
-  std::cout << calculator.Calculate(learn_bazel::BinaryOperatorType::kDivision, 1.0, 2.0) << std::endl;
+  std::cout << calculator.Calculate(learn_bazel::BinaryOperatorType::kDivision, 1.0, 0.0) << std::endl;
   std::cout << calculator.Calculate(learn_bazel::UnaryOperatorType::kMinus, 2.0) << std::endl;
 
   return EXIT_SUCCESS;
--- operators/division.h
+++ operators/division.h
@@ -4,11 +4,14 @@
 
 #include "operators/operator.h"
 
+#include "glog/logging.h"
+
 namespace learn_bazel {
 
 template <typename T> class Division : public BinaryOperator<T> {
 public:
   virtual T Calculate(const T &lhs_operand, const T &rhs_operand) const override {
+    CHECK_NE(rhs_operand, 0.0) << "Divide by zero!";
     return lhs_operand / rhs_operand;
   }
 };
